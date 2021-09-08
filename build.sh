be06f5ae19e51f7e5f6f3358fa9f8cb85d013fda
Arjun-Ingole <arjuningole38@gmail.com>
2021-09-08 23:33:01 +0600

    build/script: Update Build Script for GCC
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- build.sh
+++ build.sh
@@ -5,15 +5,25 @@
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="HydrogenKernel-miatoll-$(date '+%Y%m%d-%H%M').zip"
-TC_DIR="$HOME/tc/proton-clang"
+GCC64_DIR="$HOME/GCC/aarch64-elf"
+GCC32_DIR="$HOME/GCC/arm-eabi"
 DEFCONFIG="cust_defconfig"
 
-export PATH="$TC_DIR/bin:$PATH"
+export PATH="$GCC64_DIR/bin:$PATH"
+export PATH="$GCC32_DIR/bin/:$PATH"
 
-if ! [ -d "$TC_DIR" ]; then
-echo "Proton clang not found! Cloning to $TC_DIR..."
-if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $TC_DIR; then
-echo "Cloning failed! Aborting..."
+if ! [ -d "$GCC64_DIR" ]; then
+echo "GCC for arm64 not found! Cloning to $GCC64_DIR..."
+if ! git clone --depth=1 https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf $GCC64_DIR; then
+echo "Cloning GCC for Arm64  failed! Aborting..."
+exit 1
+fi
+fi
+
+if ! [ -d "$GCC32_DIR" ]; then
+echo "GCC for arm32 not found! Cloning to $GCC32_DIR..."
+if ! git clone --depth=1 https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_arm_arm-eabi $GCC32_DIR; then
+echo "Cloning GCC for Arm64  failed! Aborting..."
 exit 1
 fi
 fi
@@ -26,7 +36,7 @@ mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump  CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE=$GCC64_DIR/bin/aarch64-elf- CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi- Image.gz dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
