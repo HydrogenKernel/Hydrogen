f8c5effd4110772effdda0ba5e38a0f5e30718fb
Arjun-Ingole <arjuningole38@gmail.com>
2022-01-10 20:00:40 +0000

    build/script: Build with Proton clang
    - While we are at it why not configure buuld script
    
    This partially reverts commit 6bcc27443e7d5ecd37eacabc495c7a11cad09720.
    
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- build.sh
+++ build.sh
@@ -5,13 +5,19 @@
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="HydrogenKernel-miatoll-$(date '+%Y%m%d-%H%M').zip"
-GCC64_DIR="$HOME/GCC/aarch64-elf"
-GCC32_DIR="$HOME/GCC/arm-eabi"
+TC_DIR="$HOME/tc/proton-clang"
 WET="transfer"
 DEFCONFIG="cust_defconfig"
 
-export PATH="$GCC64_DIR/bin:$PATH"
-export PATH="$GCC32_DIR/bin/:$PATH"
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+echo "Proton clang not found! Cloning to $TC_DIR..."
+if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $TC_DIR; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
 
 if ! [ -a "$WET" ]; then
 echo "Setting up wetransfer"
@@ -21,37 +27,15 @@ exit 1
 fi
 fi
 
-if ! [ -d "$GCC64_DIR" ]; then
-echo "GCC for arm64 not found! Cloning to $GCC64_DIR..."
-if ! git clone --depth=1 https://github.com/Positron-Foundation/android_prebuilts_gcc_linux-x86_aarch64_aarch64-none-elf $GCC64_DIR; then
-echo "Cloning GCC for Arm64  failed! Aborting..."
-exit 1
-fi
-fi
-
-if ! [ -d "$GCC32_DIR" ]; then
-echo "GCC for arm32 not found! Cloning to $GCC32_DIR..."
-if ! git clone --depth=1 https://github.com/Positron-Foundation/android_prebuilts_gcc_linux-x86_arm_arm-none-eabi $GCC32_DIR; then
-echo "Cloning GCC for Arm64  failed! Aborting..."
-exit 1
-fi
-fi
-
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
 rm -rf out
-fi
-
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-cp out/defconfig arch/arm64/configs/$DEFCONFIG
-exit 1;
 fi
 
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE=$GCC64_DIR/bin/aarch64-none-elf- CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-none-eabi- Image.gz dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
